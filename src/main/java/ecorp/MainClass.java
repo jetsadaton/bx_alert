b1a0833a49e2eaa2754258ac8ebec09610b2db62
unknown <kidkung1412@gmail.com>
2017-08-20 01:38:36 +0700

    test

--- src/main/java/ecorp/MainClass.java
+++ src/main/java/ecorp/MainClass.java
@@ -1,17 +1,23 @@
 package ecorp;
 
-import ecorp.service.MessageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
-public class MainClass {
+public class MainClass  extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MainClass.class);
+    }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MainClass.class, args);
     }
